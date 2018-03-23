3e6435de0edbc4f28e2e59e978aa577799bcc67c
vivequeramji <vivequeramji@gmail.com>
2018-03-23 17:59:20 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
-import matplotlib.pyplot as plt
+'''
+Author: Viveque Ramji
+Purpose: Main script to bring all modules together
+
+'''
+
+import cv2
 import numpy as np
 import time
+import logging
 
-# import robot
+import robot
 import nav
 import cam
 
+
 def online(c, n):
-	c = cam.Camera()
+	'''
+	Will continuously to get depth images from camera and then plot rgb,
+	original depth, interpolated depth, and the point where the robot is told
+	to move until keyboard interrupt
+	'''
 	c.connect()
 
-	n = nav.Navigation()
-	ax1 = plt.subplot(2,2,1)
-	ax2 = plt.subplot(2,2,2)
-	ax3 = plt.subplot(2,2,3)
-
-	im1 = ax1.imshow()
-	im2 = ax2.imshow()
-	im3 = ax3.imshow()
-
-	plt.ion()
 	try:
 		while True:
 			t = time.time()
-			depth, rgb = c.get_frames(True)
+			depth, rgb = c.get_frames(1)
 			x = n.reconstruct_frame(depth)
-			max_dist = 1
+			if x is None:
+				print("Error, caannot find where to walk")
+				continue
+			max_dist = 1.2
 			frac, deg = n.obstacle_avoid(x, max_dist)
 			print("Time: ", time.time()-t)
 			if frac == None:
-				print("Error, caannot find where to walk")
-				r.move()
+				print("Error: cannot find where to walk")
+				n.plot(rgb, depth, x, 0)
+				r.test_move()
 			else:
 				print("Rotate {:.1f} fraction".format(frac))
-				r.move(forward=0.3, turn=0.6*frac)
-
-			im1.set_data(rgb)
-			im1.scatter((1+frac)*rgb.shape[1]/2, 460)
-			im2.set_data(depth)
-			im3.set_data(x)
-
+				n.plot(rgb, depth, x, (1+frac)*rgb.shape[1]/2)
+				r.test_move(forward=0.3, turn=0.6*frac)
 
 	except KeyboardInterrupt:
+		logging.warning("Main.py: KeyboardInterrupt")
 		c.disconnect()
 		pass
 
 def offline(c, n):
+	'''
+	Offline version to load saved images and plot where the robot should move
+	'''
+
 	t = time.time()
 	filename = 'npz/%d_c_5d.npz' % 1
 	
@@ -57,7 +63,7 @@ def offline(c, n):
 
 	print("Time: ", time.time()-t)
 	if frac == None:
-		print("Error, cannot find where to walk")
+		print("Error: cannot find where to walk")
 		n.plot(rgb, depth, x, 10)
 	else:
 		print("Rotate {:.1f} degree".format(deg))
@@ -66,17 +72,22 @@ def offline(c, n):
 
 
 def main():
-	c = cam.Camera()
-	n = nav.Navigation()
-	# r = robot.Robot()
+	logging.basicConfig(filename="Control_Log_{}.log".format(time.ctime()),
+                    format='%(asctime)s - %(levelname)s: %(message)s',
+                    datefmt='%I:%M:%S',
+                    level=logging.DEBUG)
 
-	PORT = '/dev/ttyUSB0'
-	BAUDERATE = 115200
+	c = cam.Camera(subSample=0.3, height_ratio=0.3)
+	n = nav.Navigation(percSamples=0.3)
+	r = robot.Robot()
+
+	# PORT = '/dev/ttyUSB0'
+	# BAUDERATE = 115200
     # r.connect(PORT,BAUDERATE)
 
-	online = False
+	on = True
 
-	if online:
+	if on:
 		online(c, n)
 	else:
 		offline(c, n)
@@ -84,3 +95,4 @@ def main():
 
 if __name__== "__main__":
   main()
+
