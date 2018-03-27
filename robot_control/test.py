4003b52de6b979b0c8c30549d75140a32d279723
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2018-03-27 17:25:40 -0400

    Full walking test sequence

--- robot_control/test.py
+++ robot_control/test.py
@@ -1,9 +1,13 @@
+"""
+Author: Jan Bernhard
+Purpose: Full test sequence for Minitaur loaded with research platform.
+"""
+
 import robot_v2
 import time
 
 def main():
     # different for every computer
-    
     PORT = '/dev/tty.usbserial-DN01QALN' 
     BAUDERATE = 115200
     TIMEOUT = 1
@@ -11,40 +15,46 @@ def main():
     obj.connect(PORT,BAUDERATE,TIMEOUT)
     connection = False # Holds received message from Minitaur
     try:
+        # Wait for successful communication to be established
         while(connection==False):
-            print("Connecting...")
-            received = obj.move() 
+            received = obj.move(height = .5) 
             connection = True if received != None else False
-            print(received)
+            print(">>> Connecting... " + str(received))
     
         print(" >>> START TEST SEQUENCE <<<")
-        # print(">>> WALK & LOOK SLIGHTLY RIGHT, UP <<<")
-        # obj.look(turn=90,tilt=35)
-        # for _ in range(30):
-        #     obj.move(forward=0.3)
-        #     time.sleep(0.1)
-        # print(">>> HIGH WALK & LOOK FROM INITIAL POSITION <<<")
-        # obj.look(turn=0,tilt=0)
-        # for _ in range(20):
-        #     obj.move(forward=0.2,height=0.3)
-        #     time.sleep(0.1)
-        print(">>> RAISE HEIGHT <<<")
-        for _ in range (20):
-            connection = obj.move(height = .3)
+        print(">>> RAISE TO WALKING HEIGHT <<<")
+        for _ in range(30):
+            connection = obj.move(height = .2)
             if connection==True:
-                print("Successful Communication")
+                print(">>> Successful Communication")
             else:
-                print("ERROR: Unsuccessful Communication")
+                print(">>> ERROR: Unsuccessful Communication")
             time.sleep(0.1)
-        print(">>> STAND <<<")
-        for _ in range (20):
-            connection = obj.move(height = .1)
+        print(">>> WALK FORWARD <<<")
+        for _ in range(20):
+            connection = obj.move(forward=0.4,height=0.2)
             if connection==True:
-                print("Successful Communication")
+                print(">>> Successful Communication")
             else:
-                print("ERROR: Unsuccessful Communication")
+                print(">>> ERROR: Unsuccessful Communication")
             time.sleep(0.1)
-        
+        print(">>> TURN RIGHT <<<")
+        for _ in range (30):
+            connection = obj.move(forward=.3,turn = .3, height = .2)
+            if connection==True:
+                print(">>> Successful Communication")
+            else:
+                print(">>> ERROR: Unsuccessful Communication")
+            time.sleep(0.1)
+        print(">>> TURN LEFT <<<")
+        for _ in range (30):
+            connection = obj.move(forward=.3,turn = -0.3, height = .2)
+            if connection==True:
+                print(">>> Successful Communication")
+            else:
+                print(">>> ERROR: Unsuccessful Communication")
+            time.sleep(0.1)
+        obj.move()
         obj.disconnect()
         print(">>> TEST COMPLETE <<<")
         
