8060e74f8dcbb7785dc083c049050c3e62c812de
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2018-03-27 14:31:26 -0400

    Updated Readme and Code Example

--- robot_control/robot_v2.py
+++ robot_control/robot_v2.py
@@ -168,17 +168,15 @@ if __name__ == '__main__':
     # PORT = '/dev/tty.usbserial-DN01QALN' # Jan's MB
     BAUDERATE = 115200
     TIMEOUT = 1
-    obj = Robot()
+    obj = robot_v2.Robot()
     obj.connect(PORT,BAUDERATE,TIMEOUT)
     try:
         print(" >>> START TEST SEQUENCE <<<")
         print(">>> WALK & LOOK SLIGHTLY RIGHT, UP <<<")
-        obj.look(turn=90,tilt=35)
         for _ in range(30):
             obj.move(forward=0.3)
             time.sleep(0.1)
         print(">>> HIGH WALK & LOOK FROM INITIAL POSITION <<<")
-        obj.look(turn=0,tilt=0)
         for _ in range(20):
             obj.move(forward=0.2,height=0.3)
             time.sleep(0.1)
